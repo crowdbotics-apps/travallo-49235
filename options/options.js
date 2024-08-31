3e5849efcd55736ea722d9584621e649fc1087b9
Crowdbotics <team@crowdbotics.com>
2024-08-31 18:27:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "travallo-49235",
+  url: "https://travallo-49235.botics.co",
+  api: "https://travallo-49235.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
